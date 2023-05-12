298ec88c3d6b0784f503498656b97ca8b74a2f45
Ahmad Jailani <32035123+ajailani4@users.noreply.github.com>
2023-05-12 18:51:06 +0700

    Update SectionTitle

--- shared/src/commonMain/kotlin/com/ajailani/booku/ui/screen/home/component/SectionTitle.kt
+++ shared/src/commonMain/kotlin/com/ajailani/booku/ui/screen/home/component/SectionTitle.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -20,7 +21,8 @@ fun SectionTitle(
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 20.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
             text = title,
