9b26883eabc6444c07c80fe8c6ff0548553072a6
Ahmad Jailani <32035123+ajailani4@users.noreply.github.com>
2023-05-14 15:10:02 +0700

    Update BookItemCard

--- shared/src/commonMain/kotlin/com/ajailani/booku/ui/screen/home/component/BookItemCard.kt
+++ shared/src/commonMain/kotlin/com/ajailani/booku/ui/screen/home/component/BookItemCard.kt
@@ -3,53 +3,68 @@ package com.ajailani.booku.ui.screen.home.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.ajailani.booku.domain.model.Volume
-import com.ajailani.booku.ui.common.component.AsyncImage
-import com.ajailani.booku.ui.common.component.loadImageBitmap
 import com.ajailani.booku.ui.theme.Grey
+import com.seiko.imageloader.rememberAsyncImagePainter
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun BookItem(volume: Volume) {
-    Column {
-        AsyncImage(
-            modifier = Modifier
-                .size(width = 130.dp, height = 190.dp)
-                .shadow(
-                    elevation = 2.dp,
-                    shape = MaterialTheme.shapes.medium
-                )
-                .clip(MaterialTheme.shapes.medium),
-            load = { loadImageBitmap(volume.volumeInfo.imageLinks.thumbnail) },
-            painterFor = { remember { BitmapPainter(it) }},
-            contentScale = ContentScale.FillBounds,
-            contentDescription = "Book cover"
-        )
+    Column(modifier = Modifier.width(130.dp)) {
+        CompositionLocalProvider {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(190.dp)
+                    .shadow(
+                        elevation = 2.dp,
+                        shape = MaterialTheme.shapes.medium
+                    )
+                    .clip(MaterialTheme.shapes.medium),
+                painter = volume.volumeInfo.imageLinks.let {
+                    if (it != null) {
+                        rememberAsyncImagePainter(volume.volumeInfo.imageLinks!!.thumbnail)
+                    } else {
+                        painterResource("image/default_book_image.jpg")
+                    }
+                },
+                contentScale = ContentScale.FillBounds,
+                contentDescription = "Book cover"
+            )
+        }
         Spacer(modifier = Modifier.height(10.dp))
         Text(
             text = volume.volumeInfo.title,
-            style = MaterialTheme.typography.subtitle1
+            style = MaterialTheme.typography.body1.copy(
+                fontWeight = FontWeight.SemiBold
+            ),
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
         )
         Spacer(modifier = Modifier.height(5.dp))
         Text(
             text = volume.volumeInfo.authors[0],
             style = MaterialTheme.typography.body2.copy(
                 color = Grey
-            )
+            ),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
         )
     }
 }
