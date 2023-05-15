754a121bffa2c4cb5a256e91aca1c35efe3c9990
Ahmad Jailani <32035123+ajailani4@users.noreply.github.com>
2023-05-15 16:09:40 +0700

    Delete UIState.kt

--- shared/src/commonMain/kotlin/com/ajailani/booku/ui/common/UIState.kt
+++
@@ -1,8 +0,0 @@
-package com.ajailani.booku.ui.common
-
-sealed class UIState<out T> {
-    object Idle : UIState<Nothing>()
-    object Loading : UIState<Nothing>()
-    data class Success<T>(val data: T?) : UIState<T>()
-    data class Error(val message: String?) : UIState<Nothing>()
-}
