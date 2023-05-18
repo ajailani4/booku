f263071fec31ff4aa97007a9d2ef7c2bd0d0a0e4
Ahmad Jailani <32035123+ajailani4@users.noreply.github.com>
2023-05-18 22:21:00 +0700

    Add VolumeDetailViewModel in desktopApp module

--- desktopApp/src/jvmMain/kotlin/di/ViewModelModule.kt
+++ desktopApp/src/jvmMain/kotlin/di/ViewModelModule.kt
@@ -4,6 +4,7 @@ import com.ajailani.booku.util.Constants
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import ui.viewmodel.HomeViewModel
+import ui.viewmodel.VolumeDetailViewModel
 import ui.viewmodel.VolumeListViewModel
 
 val viewModelModule = module {
@@ -16,6 +17,13 @@ val viewModelModule = module {
 
     single {
         VolumeListViewModel(
+            get(),
+            get(named(Constants.CoroutineDispatcher.DEFAULT_DISPATCHER))
+        )
+    }
+
+    single {
+        VolumeDetailViewModel(
             get(),
             get(named(Constants.CoroutineDispatcher.DEFAULT_DISPATCHER))
         )
