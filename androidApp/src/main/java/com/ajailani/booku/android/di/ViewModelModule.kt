8c45e130421c96e77054971f39c6ee486a2d39de
Ahmad Jailani <32035123+ajailani4@users.noreply.github.com>
2023-05-18 21:49:26 +0700

    Add VolumeDetailViewModel in androidApp module

--- androidApp/src/main/java/com/ajailani/booku/android/di/ViewModelModule.kt
+++ androidApp/src/main/java/com/ajailani/booku/android/di/ViewModelModule.kt
@@ -1,6 +1,7 @@
 package com.ajailani.booku.android.di
 
 import com.ajailani.booku.android.ui.viewmodel.HomeViewModel
+import com.ajailani.booku.android.ui.viewmodel.VolumeDetailViewModel
 import com.ajailani.booku.android.ui.viewmodel.VolumeListViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 val viewModelModule = module {
     viewModel { HomeViewModel(get()) }
     viewModel { VolumeListViewModel(get(), get()) }
+    viewModel { VolumeDetailViewModel(get(), get()) }
 }
