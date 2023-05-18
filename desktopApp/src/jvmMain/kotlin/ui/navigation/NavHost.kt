76c0db8a79ab8dd425d372001d30d0d7e3a7e0cf
Ahmad Jailani <32035123+ajailani4@users.noreply.github.com>
2023-05-18 22:21:29 +0700

    Add navigation from home screen and volume list screen to volume detail screen in desktopApp module

--- desktopApp/src/jvmMain/kotlin/ui/navigation/NavHost.kt
+++ desktopApp/src/jvmMain/kotlin/ui/navigation/NavHost.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.Crossfade
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import com.ajailani.booku.ui.screen.home.HomeScreen
+import com.ajailani.booku.ui.screen.volume_detail.VolumeDetailScreen
 import com.ajailani.booku.ui.screen.volume_list.VolumeListScreen
 import com.ajailani.booku.util.Screen
 import util.DIHelper
@@ -26,6 +27,12 @@ fun NavHost(
                         navController.navigate(
                             route = Screen.VolumeList.route,
                             arguments = mapOf("query" to it)
+                        )
+                    },
+                    onNavigateToVolumeDetail = {
+                        navController.navigate(
+                            route = Screen.VolumeDetail.route,
+                            arguments = mapOf("id" to it)
                         )
                     }
                 )
@@ -50,6 +57,31 @@ fun NavHost(
                             }
                         },
                         volumeListUiState = volumeListUiState,
+                        onNavigateUp = {
+                            navController.navigateBack()
+                        },
+                        onNavigateToVolumeDetail = {
+                            navController.navigate(
+                                route = Screen.VolumeDetail.route,
+                                arguments = mapOf("id" to it)
+                            )
+                        }
+                    )
+                }
+            }
+
+            Screen.VolumeDetail.route -> {
+                val id = NavArgumentController.getArguments(Screen.VolumeDetail.route)?.get("id")
+                val volumeDetailViewModel = DIHelper().volumeDetailViewModel
+                val volumeDetailUiState = volumeDetailViewModel.volumeDetailUiState
+
+                if (id != null) {
+                    LaunchedEffect(id) {
+                        volumeDetailViewModel.getVolumeDetail(id)
+                    }
+
+                    VolumeDetailScreen(
+                        volumeDetailUiState = volumeDetailUiState,
                         onNavigateUp = {
                             navController.navigateBack()
                         }
--- desktopApp/src/jvmMain/kotlin/util/DIHelper.kt
+++ desktopApp/src/jvmMain/kotlin/util/DIHelper.kt
@@ -3,9 +3,11 @@ package util
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import ui.viewmodel.HomeViewModel
+import ui.viewmodel.VolumeDetailViewModel
 import ui.viewmodel.VolumeListViewModel
 
 class DIHelper : KoinComponent {
     val homeViewModel by inject<HomeViewModel>()
     val volumeListViewModel by inject<VolumeListViewModel>()
+    val volumeDetailViewModel by inject<VolumeDetailViewModel>()
 }
