6ee02eadf9bc1545cb0e8475232e7fc0965da251
Ahmad Jailani <32035123+ajailani4@users.noreply.github.com>
2023-05-18 20:57:14 +0700

    Update VolumeDetailScreen in order to consume VolumeDetailUiState

--- shared/src/commonMain/kotlin/com/ajailani/booku/ui/screen/volume_detail/VolumeDetailScreen.kt
+++ shared/src/commonMain/kotlin/com/ajailani/booku/ui/screen/volume_detail/VolumeDetailScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -29,6 +30,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -41,6 +43,7 @@ import com.seiko.imageloader.rememberAsyncImagePainter
 
 @Composable
 fun VolumeDetailScreen(
+    volumeDetailUiState: VolumeDetailUiState,
     onNavigateUp: () -> Unit
 ) {
     val scaffoldState = rememberScaffoldState()
@@ -68,106 +71,136 @@ fun VolumeDetailScreen(
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
         ) {
-            Column(modifier = Modifier.padding(20.dp)) {
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(
-                        modifier = Modifier
-                            .size(width = 200.dp, height = 270.dp)
-                            .clip(MaterialTheme.shapes.large),
-                        painter = rememberAsyncImagePainter(Constants.URL.DEFAULT_BOOK_IMAGE),
-                        contentScale = ContentScale.FillBounds,
-                        contentDescription = "Volume cover"
-                    )
-                    Spacer(modifier = Modifier.height(20.dp))
-                    Text(
-                        text = "Sapiens",
-                        style = MaterialTheme.typography.h2
-                    )
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Text(
-                        text = "Yuval Noah Harari",
-                        color = Grey
-                    )
-                }
-                Spacer(modifier = Modifier.height(25.dp))
-                Card(
-                    border = BorderStroke(
-                        width = 1.dp,
-                        color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
-                    ),
-                    elevation = 0.dp
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(IntrinsicSize.Min)
-                            .padding(horizontal = 20.dp, vertical = 15.dp),
-                        horizontalArrangement = Arrangement.SpaceAround
-                    ) {
-                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                            Text(
-                                text = "Rating",
-                                style = MaterialTheme.typography.caption.copy(
-                                    color = Grey
-                                )
-                            )
-                            Spacer(modifier = Modifier.height(5.dp))
-                            Text(
-                                text = "4.2",
-                                style = MaterialTheme.typography.body1.copy(
-                                    color = MaterialTheme.colors.primary,
-                                    fontWeight = FontWeight.SemiBold
-                                )
-                            )
+            volumeDetailUiState.apply {
+                when {
+                    loading == true -> {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(top = 180.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator()
                         }
-                        Divider(modifier = Modifier.fillMaxHeight().width(1.dp))
-                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                            Text(
-                                text = "Number of pages",
-                                style = MaterialTheme.typography.caption.copy(
-                                    color = Grey
+                    }
+
+                    loading == false && errorMessage == null -> {
+                        if (volumeInfo != null) {
+                            Column(modifier = Modifier.padding(20.dp)) {
+                                Column(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
+                                    Image(
+                                        modifier = Modifier
+                                            .size(width = 200.dp, height = 270.dp)
+                                            .clip(MaterialTheme.shapes.large),
+                                        painter = rememberAsyncImagePainter(
+                                            volumeInfo.imageLinks?.thumbnail
+                                                ?: Constants.URL.DEFAULT_BOOK_IMAGE
+                                        ),
+                                        contentScale = ContentScale.FillBounds,
+                                        contentDescription = "Volume cover"
+                                    )
+                                    Spacer(modifier = Modifier.height(20.dp))
+                                    Text(
+                                        text = volumeInfo.title,
+                                        style = MaterialTheme.typography.h2
+                                    )
+                                    Spacer(modifier = Modifier.height(10.dp))
+                                    Text(
+                                        text = volumeInfo.authors?.get(0) ?: "-",
+                                        color = Grey
+                                    )
+                                }
+                                Spacer(modifier = Modifier.height(25.dp))
+                                Card(
+                                    border = BorderStroke(
+                                        width = 1.dp,
+                                        color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+                                    ),
+                                    elevation = 0.dp
+                                ) {
+                                    Row(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .height(IntrinsicSize.Min)
+                                            .padding(horizontal = 20.dp, vertical = 15.dp),
+                                        horizontalArrangement = Arrangement.SpaceAround
+                                    ) {
+                                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                            Text(
+                                                text = "Rating",
+                                                style = MaterialTheme.typography.caption.copy(
+                                                    color = Grey
+                                                )
+                                            )
+                                            Spacer(modifier = Modifier.height(5.dp))
+                                            Text(
+                                                text = volumeInfo.averageRating.let {
+                                                    if (it == null) "-" else "$it"
+                                                },
+                                                style = MaterialTheme.typography.body1.copy(
+                                                    color = MaterialTheme.colors.primary,
+                                                    fontWeight = FontWeight.SemiBold
+                                                )
+                                            )
+                                        }
+                                        Divider(modifier = Modifier.fillMaxHeight().width(1.dp))
+                                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                            Text(
+                                                text = "Number of pages",
+                                                style = MaterialTheme.typography.caption.copy(
+                                                    color = Grey
+                                                )
+                                            )
+                                            Spacer(modifier = Modifier.height(5.dp))
+                                            Text(
+                                                text = volumeInfo.pageCount.let {
+                                                    if (it == null) "-" else "$it"
+                                                },
+                                                style = MaterialTheme.typography.body1.copy(
+                                                    color = MaterialTheme.colors.primary,
+                                                    fontWeight = FontWeight.SemiBold
+                                                )
+                                            )
+                                        }
+                                        Divider(modifier = Modifier.fillMaxHeight().width(1.dp))
+                                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                            Text(
+                                                text = "Language",
+                                                style = MaterialTheme.typography.caption.copy(
+                                                    color = Grey
+                                                )
+                                            )
+                                            Spacer(modifier = Modifier.height(5.dp))
+                                            Text(
+                                                text = volumeInfo.language.uppercase(),
+                                                style = MaterialTheme.typography.body1.copy(
+                                                    color = MaterialTheme.colors.primary,
+                                                    fontWeight = FontWeight.SemiBold
+                                                )
+                                            )
+                                        }
+                                    }
+                                }
+                                Spacer(modifier = Modifier.height(25.dp))
+                                Text(
+                                    text = "Description",
+                                    style = MaterialTheme.typography.h3
                                 )
-                            )
-                            Spacer(modifier = Modifier.height(5.dp))
-                            Text(
-                                text = "450",
-                                style = MaterialTheme.typography.body1.copy(
-                                    color = MaterialTheme.colors.primary,
-                                    fontWeight = FontWeight.SemiBold
-                                )
-                            )
+                                Spacer(modifier = Modifier.height(20.dp))
+                                Text(text = volumeInfo.description ?: "-")
+                            }
                         }
-                        Divider(modifier = Modifier.fillMaxHeight().width(1.dp))
-                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                            Text(
-                                text = "Language",
-                                style = MaterialTheme.typography.caption.copy(
-                                    color = Grey
-                                )
-                            )
-                            Spacer(modifier = Modifier.height(5.dp))
-                            Text(
-                                text = "EN",
-                                style = MaterialTheme.typography.body1.copy(
-                                    color = MaterialTheme.colors.primary,
-                                    fontWeight = FontWeight.SemiBold
-                                )
-                            )
+                    }
+
+                    errorMessage != null -> {
+                        LaunchedEffect(scaffoldState) {
+                            scaffoldState.snackbarHostState.showSnackbar(errorMessage)
                         }
                     }
                 }
-                Spacer(modifier = Modifier.height(25.dp))
-                Text(
-                    text = "Description",
-                    style = MaterialTheme.typography.h3
-                )
-                Spacer(modifier = Modifier.height(20.dp))
-                Text(
-                    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut felis libero, lacinia at nulla ut, scelerisque hendrerit est. Vivamus ac volutpat lorem. Curabitur feugiat turpis vitae lorem bibendum dapibus. Quisque cursus, ante a tincidunt porta, tellus purus lobortis eros, in bibendum nunc felis sit amet metus."
-                )
             }
         }
     }
